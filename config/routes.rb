52ef170a34907133694182791673de4622829ff8
github username <github email address>
2017-10-23 15:38:28 -0400

    incomplete DO NOT MERGE

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :invites
-  resources :dinners
-  resources :courses
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :recipes
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
