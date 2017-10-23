6a823423b64923e1e785dc7316a0da094e24a84b
rebeccanewborn <rjn58@cornell.edu>
2017-10-23 14:43:01 -0400

    reconciled recipes and users crud

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,6 +16,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def index
+    @recipes = Recipe.all
+  end
+
   private
   def recipe_params
     params.require(:recipe).permit(:name, :ingredients, :instructions)
