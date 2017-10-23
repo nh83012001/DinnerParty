ddecda2b78398cf7f7b1da20ea7a4815a08f3776
github username <github email address>
2017-10-23 14:14:09 -0400

    Finished basic crud

--- DinnerParty/app/models/user.rb
+++ DinnerParty/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :dinners, through: :invites
   has_many :userrecipes
   has_many :recipes, through: :userrecipes
+
+  validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :password, length: { minimum: 6}
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
