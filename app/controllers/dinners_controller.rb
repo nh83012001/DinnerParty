57ca497b63296100ac5ffd67e7891ebe3f7ed2b0
Nick Hall <nickhall122@gmail.com>
2017-10-24 11:50:08 -0400

    created invites and make new dinner

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,2 +1,32 @@
 class DinnersController < ApplicationController
+  def index
+    @dinners = Dinner.all
+  end
+
+  def show
+    @dinner = Dinner.find(params[:id])
+  end
+
+  def new
+
+    @dinner = Dinner.new
+    @dinner.invites.build
+  end
+
+  def create
+    @dinner = Dinner.create(dinner_params)
+    params[:dinner][:user_ids].each do |id|
+      @dinner.users << User.find(id) unless id==""
+    end
+
+    redirect_to dinner_path(@dinner)
+
+  end
+
+  private
+
+  def dinner_params
+    params.require(:dinner).permit(:location, :time, :host_id, :name, :attire, :user_ids=>[])
+  end
+
 end
