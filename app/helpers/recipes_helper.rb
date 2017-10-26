0efa228cc4c16c9432b9c54e567e6c114b24b247
github username <github email address>
2017-10-26 09:34:21 -0400

    initial_commit

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,6 +1,6 @@
 module RecipesHelper
 
   def test_recipe_ownership
-    return true unless get_user_id == nil && get_user_id == recipe.owner_id
+    return true unless get_user_id == nil && get_user_id == @recipe.owner_id
   end
 end
